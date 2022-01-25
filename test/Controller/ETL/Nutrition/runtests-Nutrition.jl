cda66b881e9e9f4b7a33d20d638148235d521084
Vincent Laugier <vincent.laugier@gmail.com>
2022-01-25 11:41:45 +0100

    Improve Nutrition module

--- test/Controller/ETL/Nutrition/runtests-Nutrition.jl
+++ test/Controller/ETL/Nutrition/runtests-Nutrition.jl
@@ -1,10 +1,29 @@
 include("../../../runtests-prerequisite.jl")
 
 @testset "Test ETL.Nutrition.computeFeedingTypeVolumeAndCalories" begin
+
+    # Parenteral
     window = DataFrame(
         attributeDictionaryPropName = ["toto.intakeVolume","a","b","toto.intakeVolume"],
         verboseForm = ["Perikabiven","a","b","Perikabiven"],
         terseForm = ["10","","","10.5551"]
     )
-    @test ETL.Nutrition.computeFeedingTypeVolumeAndCalories(window) == 20.56
+    ETL.Nutrition.computeFeedingTypeVolumeAndCalories(window)
+
+    # Enteral
+    window = DataFrame(
+        attributeDictionaryPropName = ["toto.intakeVolume","a","b","toto.intakeVolume"],
+        verboseForm = ["megareal","a","b","megareal"],
+        terseForm = ["10","","","10.5551"]
+    )
+    ETL.Nutrition.computeFeedingTypeVolumeAndCalories(window)
+
+    # Natural
+    window = DataFrame(
+        attributeDictionaryPropName = ["PtDietaryOrder_","a","b","toto.bb"],
+        verboseForm = ["a","a","b","a"],
+        terseForm = ["10","","","10.5551"]
+    )
+    ETL.Nutrition.computeFeedingTypeVolumeAndCalories(window)
+
 end
