cf2f346b5c474aff931adbbe2b61e5e4bcc54edd
Vincent Laugier <vincent.laugier@gmail.com>
2022-06-29 10:09:20 +0200

    src/Controller/Scheduler/
    Add Scheduler

---
+++ src/Controller/Scheduler/Scheduler-def.jl
@@ -0,0 +1,7 @@
+function checkIfAnythingNeedsToBeExecuted end
+function checkIfNeedsToExecuteFunction end
+
+
+every2Minutes = collect(Time("00:00:00"):Minute(2):Time("23:59:00"))
+every5Minutes = collect(Time("00:00:00"):Minute(5):Time("23:59:00"))
+every60Minutes = collect(Time("00:00:00"):Minute(60):Time("23:59:00"))
